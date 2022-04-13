215541d60509c17552b315c31514621a04b7b859
Jorge Emmanuel González Hernández <jgonzalez@correo.uam.mx>
2022-04-12 21:07:51 -0500

    Clase 2 - Agregando artefactos al proyecto

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ProductsComponent } from './pages/products/products.component';
+import { CartComponent } from './pages/cart/cart.component';
+
+const routes: Routes = [{
+  path: 'productos',
+  component: ProductsComponent
+},{
+  path: 'cart',
+  component: CartComponent
+}];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { AppRoutingModule } from './app-routing.module';
+import { CartComponent } from './pages/cart/cart.component';
+import { ButtonsComponent } from './components/buttons/buttons.component';
+import { CardComponent } from './components/card/card.component';
+import { DescriptionComponent } from './components/description/description.component';
+import { ImagenComponent } from './components/imagen/imagen.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    CartComponent,
+    ButtonsComponent,
+    CardComponent,
+    DescriptionComponent,
+    ImagenComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
